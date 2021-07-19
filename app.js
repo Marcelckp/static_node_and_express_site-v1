e86f6c6537c39bcfe3d99a22d287b980c2c62339
Marcelckp <marcelkpalmer@gmail.com>
2021-07-19 19:45:20 +0200

    will update up the data.json file and pug template ('project.pug')

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const data = require('./data.json');
 const app = express();
-const createError = require('http-errors')
+const createError = require('http-errors');
 
+//view engine & static file link
 app.set('view engine', 'pug');
 app.use('/static', express.static('public'));
 
@@ -10,23 +11,26 @@ app.use('/static', express.static('public'));
 //    Routes
 //--------------
 
+//route for the home route of the express app
 app.get('/', (req, res) => {
-    const dataP = data.projects
-    res.render('index', { dataP })
-})
+    const dataP = data.projects;
+    res.render('index', { dataP });
+});
 
+//route for the about section of the portfolio Express app
 app.get('/about', (req, res) => {
-    const dataP = data.projects
-    res.render('about', { dataP })
-})
+    const dataP = data.projects;
+    res.render('about', { dataP });
+});
 
+//route for projects with the ability to insert a param in the url and access a specific project
 app.get('/projects/:id', (req, res) => {
     const id = parseInt(req.params.id);
     const dataP = data.projects[id];
 
     // console.log(dataP)
-    res.render('project', { dataP })
-})
+    res.render('project', { dataP });
+});
 
 
 
@@ -37,8 +41,8 @@ app.get('/projects/:id', (req, res) => {
 
 app.use((req, res, next) => {
     // console.log('404 error handler has been called')
-    next(createError(404))
-})
+    next(createError(404));
+});
 
 //Global Handler
 
@@ -46,16 +50,16 @@ app.use((err, req, res, next) => {
     // if (err) console.log('Global Handler has been called')
 
     if (res.status === 404) {
-        res.status(404)
-        res.render('not-found.pug', { err })
+        res.status(404);
+        res.render('not-found.pug', { err });
     } else {
-        err.message = err.message || 'There was an error processing your request!'
-        err.status = err.status || 500
-        res.render('error', { err })
+        err.message = err.message || 'There was an error processing your request!';
+        err.status = err.status || 500;
+        res.render('error', { err });
     }
-})
+});
 
 
 app.listen(3000, () => {
-    console.log('Server listing on port 3000\n')
-})
+    console.log('Server listing on port 3000\n');
+});
